56b921f5894a5ae5d38f95f1e35c23a2f44f933a
jmsmg <jmsmg1@me.com>
2023-08-31 22:12:51 +0900

    DFS OK

--- check_map.c
+++ check_map.c
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 17:48:07 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/08/31 21:01:47 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/08/31 22:11:48 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 void	ft_find_route(int x, int y, char *map, t_info *info)
 {
-	if (map[x + (y * info->width)] == '1' || map[x * (y * info->width)] == 'V')
-		return ;
-	if (map[(y * info->width)] == 'C')
-		info->c_cnt++;
-	if (map[(y * info->width)] == 'E')
+	if (map[x + (y * info->width)] == 'E')
 		info->e_cnt++;
-	map[x * (y * info->width)] = 'V';
+	if (map[x + (y * info->width)] == 'C')
+		info->c_cnt++;
+	if (map[x + (y * info->width)] == '1' || map[x + (y * info->width)] == 'V'
+		|| map[x + (y * info->width)] == 'E')
+		return ;
+	map[x + (y * info->width)] = 'V';
 	ft_find_route(x - 1, y, map, info);
 	ft_find_route(x, y - 1, map, info);
 	ft_find_route(x + 1, y, map, info);
@@ -40,32 +41,30 @@ void	ft_find_player(t_info *info)
 	info->y = i / info->width;
 }
 
-void	ft_check_char(t_info *info)
+void	ft_check_char(t_info *info, t_char_cnt *char_cnt)
 {
 	int			i;
 	char		c;
-	t_char_cnt	char_cnt;
 
 	i = 0;
-	char_cnt = (t_char_cnt){.c = 0, .e = 0, .p = 0};
 	while ((info->map)[i])
 	{
 		c = (info->map)[i];
 		if (!(c == '0' || c == '1' || c == 'C' || c == 'E' || c == 'P'))
 			ft_error(CHAR_ERR);
 		if (c == 'C')
-			char_cnt.c += 1;
+			char_cnt->c += 1;
 		else if (c == 'E')
-			char_cnt.e += 1;
+			char_cnt->e += 1;
 		else if (c == 'P')
-			char_cnt.p += 1;
+			char_cnt->p += 1;
 		i++;
 	}
-	if (char_cnt.p != 1)
+	if (char_cnt->p != 1)
 		ft_error(PLAYER_ERR);
-	else if (char_cnt.e != 1)
+	else if (char_cnt->e != 1)
 		ft_error(EXIT_ERR);
-	else if (char_cnt.c < 1)
+	else if (char_cnt->c < 1)
 		ft_error(COLLECT_ERR);
 }
 
@@ -98,7 +97,9 @@ int	ft_check_wall(t_info *info)
 void	ft_check_map(t_info *info)
 {
 	char	*map;
+	t_char_cnt	char_cnt;
 
+	char_cnt = (t_char_cnt){.c = 0, .e = 0, .p = 0};
 	info->c_cnt = 0;
 	info->e_cnt = 0;
 	map = ft_strdup(info->map);
@@ -108,10 +109,10 @@ void	ft_check_map(t_info *info)
 		ft_error(RECTAGLE_ERR);
 	if (!ft_check_wall(info))
 		ft_error(WALL_FAIL);
-	ft_check_char(info);
+	ft_check_char(info, &char_cnt);
 	ft_find_player(info);
 	ft_find_route(info->x, info->y, map, info);
-	if (info->c_cnt == 0 || info->e_cnt == 0)
+	if (info->c_cnt != char_cnt.c || info->e_cnt != char_cnt.e)
 		ft_error(COLLECT_ERR);
 	free(map);
 }
