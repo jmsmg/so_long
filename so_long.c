3564330279c2ebd2ee0cd17746ae8e77f5b2dcc6
jmsmg <jmsmg1@me.com>
2023-08-31 21:28:06 +0900

    에러 메시지 서브젝트에 맞게 출력

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 10:46:46 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/08/30 17:23:42 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/08/31 20:44:59 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,29 @@
 int	ft_error(int flag)
 {
 	if (flag == MALLOC_FAIL)
-		write(1, "malloc fail error\n", 18);
+		write(1, "Error\nmalloc fail error\n", 24);
+	else if (flag == OPEN_FAIL)
+		write(1, "Error\nThe file could not be opened.\n", 36);
 	else if (flag == WALL_FAIL)
-		write(1, "Map must be surrounded by walls\n", 32);
+		write(1, "Error\nMap must be surrounded by walls\n", 38);
 	else if (flag == INCORRECT_ARGC)
-		write(1, "Incorrect number of arguments, it's not 2\n", 42);
+		write(1, "Error\nIncorrect number of arguments, it's not 2\n", 48);
 	else if (flag == FILE_FORMAT_ERROR)
-		write(1, "The file format is incorrect.\n", 30);
+		write(1, "Error\nThe file format is incorrect.\n", 36);
 	else if (flag == RECTAGLE_ERR)
-		write(1, "Is not Rectangle\n", 17);
+		write(1, "Error\nIs not Rectangle\n", 23);
 	else if (flag == CHAR_ERR)
-		write(1, "Incorrect character\n", 20);
+		write(1, "Error\nIncorrect character\n", 26);
 	else if (flag == PLAYER_ERR)
-		write(1, "player error\n", 13);
+		write(1, "Error\nplayer error\n", 19);
 	else if (flag == EXIT_ERR)
-		write(1, "exit error\n", 11);
+		write(1, "Error\nexit error\n", 17);
 	else if (flag == COLLECT_ERR)
-		write(1, "collect error\n", 14);
+		write(1, "Error\ncollect error\n", 20);
 	exit(1);
 }
 
+/*
 int	deal_key(int key, t_coordinate *coordinate)
 {
 	if (key == KEY_W)
@@ -59,12 +62,7 @@ int	deal_key(int key, t_coordinate *coordinate)
 	}
 	return (0);
 }
-
-void	init_coordinate(t_coordinate	*coordinate)
-{
-	coordinate->x = 3;
-	coordinate->y = 4;
-}
+*/
 
 int	main(int argc, char *argv[])
 {
@@ -78,6 +76,7 @@ int	main(int argc, char *argv[])
 	ft_check_map(info);
 	ft_set_info(info);
 	ft_draw_map(info, info->mlx, info->win);
-	mlx_loop(info->mlx);
+	// mlx_key_hook(info->win, &deal_key, info);
+	// mlx_loop(info->mlx);
 	return (0);
 }
